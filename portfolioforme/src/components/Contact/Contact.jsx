d253b18aef6e44ddaaf0e284dd8d0952d1c8b12c
difelk <dilshan.f@theavo.net>
2024-12-14 23:30:42 +0530

    added basic structure

--- portfolioforme/src/components/Contact/Contact.jsx
+++ portfolioforme/src/components/Contact/Contact.jsx
@@ -3,8 +3,35 @@ import './Contact.scss';
 
 const Contact = () => {
   return (
-    <section id="contact" className="contact-section">
-      <h2>Contact Me</h2>
+    <section id="about" className="me-5">
+      {/* <h1 className='about-me-dec'>About Me</h1>
+      <div className='desc'>
+          <p className='about-me-dec'>I have more than 3 years of experience in Software Engineering field. I’m an enthusiastic and ambitious person
+who passionate of learning new technologies while taking up new challenges. As being a good team player and
+quick learner. I’m willing to serve for an organization allowing me to learn, develop my skills, utilize my
+knowledge and experience through working with a team to pursue common goals deliver the best results to the
+organization and society while advancing my career.</p>
+        </div> */}
+
+      <div className="section_left">
+        <div className="circle_img">
+          <h2 className="header_about">Contact Me</h2>
+        </div>
+      </div>
+      <div className="section_right">
+        {" "}
+        <p className="about-me-dec">
+          I have more than 3 years of experience in Software Engineering field.
+          I’m an enthusiastic and ambitious person who passionate of learning
+          new technologies while taking up new challenges. As being a good team
+          player and quick learner. I’m willing to serve for an organization
+          allowing me to learn, develop my skills, utilize my knowledge and
+          experience through working with a team to pursue common goals deliver
+          the best results to the organization and society while advancing my
+          career.
+        </p>
+        <button className="contact_button">Contact Me</button>
+      </div>
     </section>
   );
 };
