113f1f38a6bd7d10e122a24450cd821eb79844a7
milan <milan.m@theavo.net>
2024-12-14 22:24:03 +0530

    added some contents

--- portfolioforme/src/components/Portfolio/Portfolio.jsx
+++ portfolioforme/src/components/Portfolio/Portfolio.jsx
@@ -3,8 +3,15 @@ import './Portfolio.scss';
 
 const Portfolio = () => {
   return (
-    <section id="portfolio" className="portfolio-section">
-      <h2>My Portfolio</h2>
+    <section id="about" className="desc">
+      <h1 className='about-me-dec'>Experience</h1>
+      <div className='desc'>
+          <p className='about-me-dec'>I have more than 3 years of experience in Software Engineering field. I’m an enthusiastic and ambitious person
+who passionate of learning new technologies while taking up new challenges. As being a good team player and
+quick learner. I’m willing to serve for an organization allowing me to learn, develop my skills, utilize my
+knowledge and experience through working with a team to pursue common goals deliver the best results to the
+organization and society while advancing my career.</p>
+        </div>
     </section>
   );
 };
