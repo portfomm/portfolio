72a171f9e2edced568039e768dd74290bf0d446f
difelk <dilshan.f@theavo.net>
2024-12-15 17:09:00 +0530

    ui v04

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -3,35 +3,20 @@ import './Contact.scss';
 
 const Contact = () => {
   return (
-    <section id="about" className="me-5">
-      {/* <h1 className='about-me-dec'>About Me</h1>
-      <div className='desc'>
-          <p className='about-me-dec'>I have more than 3 years of experience in Software Engineering field. I’m an enthusiastic and ambitious person
-who passionate of learning new technologies while taking up new challenges. As being a good team player and
-quick learner. I’m willing to serve for an organization allowing me to learn, develop my skills, utilize my
-knowledge and experience through working with a team to pursue common goals deliver the best results to the
-organization and society while advancing my career.</p>
-        </div> */}
+    <section id="contact" className="me-5">
+       <h2 className="heading">Contact Me</h2>
+      <form className="contact-form">
+        <label htmlFor="name">Name *</label>
+        <input type="text" placeholder='Name' id="name" name="name" required />
 
-      <div className="section_left">
-        <div className="circle_img">
-          <h2 className="header_about">Contact Me</h2>
-        </div>
-      </div>
-      <div className="section_right">
-        {" "}
-        <p className="about-me-dec">
-          I have more than 3 years of experience in Software Engineering field.
-          I’m an enthusiastic and ambitious person who passionate of learning
-          new technologies while taking up new challenges. As being a good team
-          player and quick learner. I’m willing to serve for an organization
-          allowing me to learn, develop my skills, utilize my knowledge and
-          experience through working with a team to pursue common goals deliver
-          the best results to the organization and society while advancing my
-          career.
-        </p>
-        <button className="contact_button">Contact Me</button>
-      </div>
+        <label htmlFor="phone">Phone *</label>
+        <input type="tel" placeholder='Phone'  id="phone" name="phone" required />
+
+        <label htmlFor="message">Message *</label>
+        <textarea id="message" placeholder='message'   name="message" required></textarea>
+
+        <button type="submit" className='submit-button'>Submit</button>
+      </form>
     </section>
   );
 };
--- frontend/src/components/Portfolio/Portfolio.jsx
+++ frontend/src/components/Portfolio/Portfolio.jsx
@@ -19,7 +19,7 @@ const Portfolio = () => {
         <h2 className="project_heading">My Projects</h2>
         <div className="w-100 d-flex flex-column align-item-center justify-center">
           {projects.map((project, index) => (
-            <div className="w-100 d-flex"  style={{
+            <div className={`w-100 d-flex ${'project-item'.concat(index % 2 === 0 ? '-left' : '-right')}`}  style={{
               justifyContent: index % 2 === 0 ? "flex-start" : "flex-end",
             }}>
             <div
