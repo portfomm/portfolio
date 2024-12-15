cef62d1cc6735129cbf0ea241fa2b3210e1d76dc
milan <milan.m@theavo.net>
2024-12-15 19:23:16 +0530

    changes in contact me

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Contact.scss';
+import GmailIcon from '../../assets/icons/gmail';
+import LinkedInIcon from '../../assets/icons/linkedin';
+import GitHub from '../../assets/icons/github';
 
 const Contact = () => {
   return (
     <section id="contact" className="me-5">
        <h2 className="heading">Contact Me</h2>
       <form className="contact-form">
-        <label htmlFor="name">Full Name *</label>
+        {/* <label htmlFor="name">Full Name *</label>
         <input type="text" placeholder='Enter Full Name' id="name" name="name" required />
 
         <label htmlFor="phone">Email Address *</label>
@@ -15,7 +18,36 @@ const Contact = () => {
         <label htmlFor="message">Message *</label>
         <textarea id="message" placeholder='message'   name="message" required></textarea>
 
-        <button type="submit" className='submit-button'>Submit</button>
+        <button type="submit" className='submit-button'>Submit</button> */}
+        <a
+        href="mailto:milanmalshika22@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="custom-button"
+      >
+        <GmailIcon size={30}  color={"#000000"} />
+        Email Me
+      </a>
+
+      {/* LinkedIn Button */}
+      <a
+        href="https://www.linkedin.com/in/milan-malshika-12a9861a1/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="custom-button"
+      >
+        <LinkedInIcon size={40} color={"#000000"}/>
+        LinkedIn
+      </a>
+      <a
+        href="https://github.com/milanmalshika/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="custom-button"
+      >
+        <GitHub size={40} color={"#000000"}/>
+        GitHub
+      </a>
       </form>
     </section>
   );
